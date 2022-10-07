948ef4f171f6f9efc0d7727566e5456c523f0abf
Kmschre <102772217+Kmschre@users.noreply.github.com>
2022-10-07 13:40:24 -0700

    starting new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+
+
+let quotesDBUrl = "https://raw.githubusercontent.com/benhoneywill/stoic-quotes/master/data/quotes.json"
+
 
 function App() {
+  const [quote, setQuote] = useState("Day by day, what you do is who you become.")
+  const [author, setAuthor] = useState('Heraclitus')
+  const [quotesArray, setQuotesArray] = useState
+  (null)
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJSON = await response.json()
+    setQuotesArray(parsedJSON.quotes)
+  }
+  
+  useEffect(()=> {
+    fetchQuotes(quotesDBUrl)
+  })
+
+  const randomizeQuote = () => {
+    let vaultSize = quotesArray.length;
+    let randomIndex = Math.floor(Math.random() * vaultSize);
+    let randomQuoteInfo = quotesArray[randomIndex];
+
+     setQuote(randomQuoteInfo.text);
+     setAuthor(randomQuoteInfo.author);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <wrapper id="quote-box">
+        <p id="text">
+          "{quote}"
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          - {author}
+        </p>
+        <button onClick={() => randomizeQuote()}>New Quote</button>
+        
+        </wrapper>
       </header>
     </div>
   );
